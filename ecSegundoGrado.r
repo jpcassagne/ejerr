60c3796bcdc213ac66549c50fb945f53f2b241dd
jpcassagne <jpcassagne93@gmail.com>
2019-12-02 00:28:15 -0300

    mi primer commit

---
+++ ecSegundoGrado.r
@@ -0,0 +1,12 @@
+#Guion para resolver ecuaciones de segundo grado
+
+#Las variables
+va = 3
+vb = 2
+vc = -2
+
+#Calculo de la primera raiz
+r1 = (-vb + sqrt(vb^2 - 4 * va * vc))/(2 * va)
+
+#Calculo de la segunda raiz
+r2 = (-vb - sqrt(vb^2 - 4 * va * vc))/(2 * va)
