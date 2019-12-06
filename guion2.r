8e018f9451ff3e257c60806e2bac1bb73f54f026
jpcassagne <jpcassagne93@gmail.com>
2019-12-06 02:22:09 -0300

    guion2: data.frame y expresiones lineales

---
+++ guion2.r
@@ -0,0 +1,11 @@
+edad=c(1,2,3,5,7,9,11,13)
+altura=c(76,11,86,45,95,27,109,18,122,03,133,73,143,73,156,41)
+
+datos1 = data.frame(edad,altura)
+plot(datos1)
+
+regr_edad_altura=lm(altura~edad,data=datos1)
+
+summary(regr_edad_altura)
+
+abline(regr_edad_altura,col="red")
